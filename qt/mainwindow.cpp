0866b3755a404c6faa433aa1cd5779e86ed3d613
Eduard Ghenea <edymare97@yahoo.com>
2024-11-03 11:56:15 +0200

    getting a templace project to start with

--- qt/mainwindow.cpp
+++ qt/mainwindow.cpp
@@ -1,8 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
 #include <QFileDialog>
-#include <QPainter>
+#include <QColorDialog>
 #include <QMouseEvent>
 #include <QDateTime>
 
@@ -11,23 +10,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-    textSize = 32;
-    ui->sizeSlider->setRange(1, 200);
-    ui->sizeSlider->setValue(textSize);
-    displayTextSizeToLabel(textSize);
-
-    textThickness = 20;
-    ui->thicknessSlider->setRange(1, 25);
-    ui->thicknessSlider->setValue(textThickness);
-    displayTextThicknessToLabel(textThickness);
-
-    textPosX = 0;
-    textPosY = 0;
-    ui->textXSlider->setRange(0,0);
-    ui->textYSlider->setRange(0,0);
-    textColor = Qt::white;
-    changeUserInputValue("Sample Text");
+    ui->imageLabel->setScaledContents(true);
     toggleButtonsAvailability(false);
 }
 
@@ -36,178 +19,103 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::mousePressEvent(QMouseEvent *event)
-{
-    if (ui->imageLabel->geometry().contains(event->pos())) {
-
-        QPoint localPos = ui->imageLabel->mapFromParent(event->pos());
-
-        int maxWidth = ui->imageLabel->width();
-        int maxHeight = ui->imageLabel->height();
-
-        int scaleX = 1000;
-        int scaleY = 1000;
-        double progressionFactor = 1.5;
-
-        int progressiveX = static_cast<int>(pow(static_cast<double>(localPos.x()) / maxWidth, progressionFactor) * scaleX);
-        int progressiveY = static_cast<int>(pow(static_cast<double>(localPos.y()) / maxHeight, progressionFactor) * scaleY);
-
-        ui->textXSlider->setValue(progressiveX);
-        ui->textYSlider->setValue(progressiveY);
-    }
-}
-
-
-void MainWindow::saveImage() {
-    QString dateTimeNow = QDateTime::currentDateTime().toString("yyyy-MM-dd_HH-mm-ss");
-    std::string fileName = "T03.Image_" + dateTimeNow.toStdString() + ".png";
-    QString savePath = QFileDialog::getSaveFileName(this, "Save Image", QString::fromStdString(fileName), "PNG Files (*.png)");
-
-    if (!savePath.isEmpty()) {
-        std::string filePath = savePath.toStdString();
-        cv::imwrite(filePath, modifiedMat);
-    }
-}
-
 void MainWindow::loadImage() {
-    QString filePath = QFileDialog::getOpenFileName(this, tr("Open image"), ".", tr("Image FIles (*.png *.jpg *.jpeg *.bmp)"));
+    QString filePath = QFileDialog::getOpenFileName(this, tr("Open image"), ".", tr("Image Files (*.png *.jpg *.jpeg *.bmp)"));
     if (!filePath.isEmpty()) {
         originalMat = cv::imread(filePath.toStdString());
         modifiedMat = originalMat.clone();
-        updateSlidersWithMatSize(modifiedMat);
         displayImageToImageLabel(modifiedMat);
+        toggleButtonsAvailability(true);
     }
-    toggleButtonsAvailability(true);
 }
 
-void MainWindow::setTextPosition(int x, int y) {
-    textPosX = x;
-    textPosY = y;
+void MainWindow::saveImage() {
+    QString savePath = QFileDialog::getSaveFileName(this, "Save Image", "", "PNG Files (*.png)");
+    if (!savePath.isEmpty()) {
+        cv::imwrite(savePath.toStdString(), modifiedMat);
+    }
 }
 
-void MainWindow::updateText()
-{
-    userText = ui->textEdit->text();
-    updateTextAttributes();
-}
-
-void MainWindow::updateTextAttributes() {
-    textSize = ui->sizeSlider->value();
-    textThickness = ui->thicknessSlider->value();
-
-    modifiedMat = originalMat.clone();
-    cv::Scalar cvTextColor(textColor.blue(), textColor.green(), textColor.red());
-    int fontFace = cv::FONT_HERSHEY_SIMPLEX;
-
-    cv::putText(modifiedMat, userText.toStdString(), cv::Point(textPosX, textPosY),
-                fontFace, textSize / 20.0, cvTextColor, textThickness);
-
-    displayImageToImageLabel(modifiedMat);
-}
-
-void MainWindow::updateSlidersWithMatSize(const cv::Mat &mat) {
-    if (mat.empty()) return;
-
-    ui->textXSlider->setRange(10, mat.cols - 10); // Range from 10 to width - 10
-    ui->textXSlider->setValue(textPosX);
-    displayTextToXLabel(textPosX);
-
-    ui->textYSlider->setRange(10, mat.rows - 10); // Range from 10 to height - 10
-    ui->textYSlider->setValue(textPosY);
-    displayTextToYLabel(textPosY);
-}
-
-void MainWindow::changeUserInputValue(std::string value){
-    userText = QString::fromStdString(value);
-    ui->textEdit->setText(userText);
-}
-
-void MainWindow::toggleButtonsAvailability(bool value) {
-    ui->saveButton->setDisabled(!value);
-    ui->colorButton->setDisabled(!value);
-    ui->textEdit->setDisabled(!value);
-}
-
-
-void MainWindow::on_saveButton_clicked()
-{
-    saveImage();
-}
-
-void MainWindow::on_loadButton_clicked()
-{
+void MainWindow::on_loadButton_clicked() {
     loadImage();
 }
 
-void MainWindow::on_colorButton_clicked()
-{
-    QColor selectedColor = QColorDialog::getColor(textColor, this, tr("Select Text Color"));
+void MainWindow::on_saveButton_clicked() {
+    saveImage();
+}
 
+void MainWindow::on_colorButton_clicked() {
+    QColor selectedColor = QColorDialog::getColor(Qt::white, this, "Select Background Color");
     if (selectedColor.isValid()) {
-        textColor = selectedColor;
-        updateText();
+        cv::Scalar color(selectedColor.blue(), selectedColor.green(), selectedColor.red());
+        replaceBackgroundWithColor(color);
     }
 }
 
-
-void MainWindow::on_sizeSlider_valueChanged(int value)
-{
-    textSize = value;
-    updateText();
-    displayTextSizeToLabel(value);
+void MainWindow::on_imageButton_clicked() {
+    QString filePath = QFileDialog::getOpenFileName(this, tr("Open background image"), ".", tr("Image Files (*.png *.jpg *.jpeg *.bmp)"));
+    if (!filePath.isEmpty()) {
+        cv::Mat bgImage = cv::imread(filePath.toStdString());
+        replaceBackgroundWithImage(bgImage);
+    }
 }
 
-void MainWindow::on_thicknessSlider_valueChanged(int value)
-{
-    textThickness = value;
-    updateText();
-    displayTextThicknessToLabel(value);
+void MainWindow::replaceBackgroundWithColor(const cv::Scalar &color) {
+    if (originalMat.empty()) return;
+
+    // Split the image into R, G, and B channels
+    std::vector<cv::Mat> channels;
+    cv::split(originalMat, channels);
+
+    // Calculate histograms for each channel
+    cv::Mat histR, histG, histB;
+    int histSize = 256;  // Number of bins
+    float range[] = {0, 256};  // The range for intensity values
+    const float *histRange = {range};
+    cv::calcHist(&channels[2], 1, 0, cv::Mat(), histR, 1, &histSize, &histRange);  // Red
+    cv::calcHist(&channels[1], 1, 0, cv::Mat(), histG, 1, &histSize, &histRange);  // Green
+    cv::calcHist(&channels[0], 1, 0, cv::Mat(), histB, 1, &histSize, &histRange);  // Blue
+
+    // Define thresholds based on histogram peaks
+    int lowThresh = 0;
+    int highThresh = 100;  // You can dynamically adjust based on histogram data for a more adaptive approach
+
+    // Create a mask based on color range derived from histograms
+    cv::Mat mask;
+    cv::inRange(originalMat, cv::Scalar(lowThresh, lowThresh, lowThresh),
+                cv::Scalar(highThresh, highThresh, highThresh), mask);
+
+    // Replace the background with the selected color
+    modifiedMat = originalMat.clone();
+    modifiedMat.setTo(color, mask);
+    displayImageToImageLabel(modifiedMat);
 }
 
-void MainWindow::on_textEdit_textChanged(const QString &value)
-{
-    userText = value;
-    updateText();
+void MainWindow::replaceBackgroundWithImage(const cv::Mat &bgImage) {
+    if (originalMat.empty() || bgImage.empty()) return;
+
+    cv::Mat resizedBg;
+    cv::resize(bgImage, resizedBg, originalMat.size());
+
+    // Use the same histogram-based mask to blend with the background image
+    cv::Mat mask;
+    int lowThresh = 0;
+    int highThresh = 100;
+    cv::inRange(originalMat, cv::Scalar(lowThresh, lowThresh, lowThresh),
+                cv::Scalar(highThresh, highThresh, highThresh), mask);
+
+    modifiedMat = originalMat.clone();
+    resizedBg.copyTo(modifiedMat, mask);
+    displayImageToImageLabel(modifiedMat);
 }
 
-void MainWindow::on_textXSlider_valueChanged(int value)
-{
-    textPosX = value;
-    updateText();
-    displayTextToXLabel(value);
+void MainWindow::displayImageToImageLabel(const cv::Mat &matImage) {
+    QImage img(matImage.data, matImage.cols, matImage.rows, matImage.step, QImage::Format_RGB888);
+    ui->imageLabel->setPixmap(QPixmap::fromImage(img.rgbSwapped()).scaled(ui->imageLabel->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
 }
 
-void MainWindow::on_textYSlider_valueChanged(int value)
-{
-    textPosY = value;
-    updateText();
-    displayTextToYLabel(value);
+void MainWindow::toggleButtonsAvailability(bool value) {
+    ui->saveButton->setEnabled(value);
+    ui->colorButton->setEnabled(value);
+    ui->imageButton->setEnabled(value);
 }
-
-
-void MainWindow::displayTextSizeToLabel(int value){
-    ui->textSizeLabel->setText(QString("Text Size: %1").arg(value));
-}
-
-void MainWindow::displayTextThicknessToLabel(int value){
-    ui->textThicknessLabel->setText(QString("Text Size: %1").arg(value));
-}
-
-void MainWindow::displayTextToXLabel(int value){
-    ui->textXLabel->setText("Text X: " + QString::number(value));
-
-}
-
-void MainWindow::displayTextToYLabel(int value){
-    ui->textYLabel->setText("Text Y: " + QString::number(value));
-}
-
-void MainWindow::displayImageToImageLabel(const cv::Mat& matImage) {
-    ui->imageLabel->setPixmap(CvMatToQPixmap(matImage).scaled(ui->imageLabel->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
-}
-
-QPixmap MainWindow::CvMatToQPixmap(const cv::Mat &mat) {
-    QImage img(mat.data, mat.cols, mat.rows, mat.step, QImage::Format_RGB888);
-    return QPixmap::fromImage(img.rgbSwapped());
-}
-
--- qt/mainwindow.h
+++ qt/mainwindow.h
@@ -3,12 +3,6 @@
 
 #include <opencv2/opencv.hpp>
 #include <QMainWindow>
-#include <QLabel>
-#include <QPixmap>
-#include <QSlider>
-#include <QLineEdit>
-#include <QPushButton>
-#include <QColorDialog>
 
 namespace Ui {
 class MainWindow;
@@ -24,64 +18,22 @@ public:
 
 private slots:
     void loadImage();
-    void updateText();
     void saveImage();
-
-    void setTextPosition(int x, int y);
-    void updateTextAttributes();
-
-    void on_saveButton_clicked();
-
     void on_loadButton_clicked();
-
+    void on_saveButton_clicked();
     void on_colorButton_clicked();
+    void on_imageButton_clicked();
 
-    void on_sizeSlider_valueChanged(int value);
-
-    void on_thicknessSlider_valueChanged(int value);
-
-    void on_textEdit_textChanged(const QString &arg1);
-
-    void on_textXSlider_valueChanged(int value);
-
-    void on_textYSlider_valueChanged(int value);
-
-protected:
-    void mousePressEvent(QMouseEvent *event) override;
 
 private:
     Ui::MainWindow *ui;
-
-    QLabel imageLabel;
-    QLabel textSizeLabel;
-    QLabel textThicknessLabel;
     cv::Mat originalMat;
     cv::Mat modifiedMat;
 
-    QLineEdit textEdit;
-    QSlider sizeSlider;
-    QSlider thicknessSlider;
-    QSlider textXSlider;
-    QSlider textYSlider;
-    QPushButton colorButton;
-    QPushButton loadButton;
-    QPushButton saveButton;
-
-    QColor textColor;
-    QString userText;
-    int textSize;
-    int textThickness;
-    int textPosX, textPosY;
-    void displayImageToImageLabel(const cv::Mat& matImage);
+    void replaceBackgroundWithColor(const cv::Scalar &color);
+    void replaceBackgroundWithImage(const cv::Mat &bgImage);
+    void displayImageToImageLabel(const cv::Mat &matImage);
     void toggleButtonsAvailability(bool value);
-    void displayTextSizeToLabel(int value);
-    void displayTextThicknessToLabel(int value);
-    void changeUserInputValue(std::string value);
-
-    void displayTextToXLabel(int value);
-    void displayTextToYLabel(int value);
-    QPixmap CvMatToQPixmap(const cv::Mat &mat) ;
-    void updateSlidersWithMatSize(const cv::Mat &mat);
 };
 
 #endif // MAINWINDOW_H
